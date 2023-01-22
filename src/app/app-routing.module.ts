5495c4e24ca568b2631de6b887da0eb3627e5c3f
PercyEdgar <1percyqq@gmail.com>
2023-01-22 18:37:41 -0500

    Actividad 21 Ángular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CanactivateGuard } from './canactivate.guard';
+import { HomeComponent } from './principal/home/home.component';
+import { LoginComponent } from './principal/login/login.component';
+import { MenuComponent } from './principal/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent, canActivate: [CanactivateGuard]},
+  {path: 'home', component: MenuComponent},
+
+{path: 'login', component: LoginComponent },
+{path: '', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
